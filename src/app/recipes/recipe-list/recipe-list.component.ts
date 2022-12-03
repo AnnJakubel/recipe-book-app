04c6ab4dec34cda475a89ab6cf9d509c29489b3a
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-03 18:01:59 +0200

    Header, Recipe, RecipeList
    HTML files mostly

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-    
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80')
+    ];
 }
