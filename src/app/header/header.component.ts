4e6572799686354bf4ac4e430f8db6b613ff471d
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-07 14:19:57 +0200

    Adding Navigation with Event Binding and ngIF
    All logic commented for personal reference

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>(); 
+  // featureSelected can be used in other components' html files by (featureSelected)="otherFunction($event)" ...
+  // @Output enables this and via $event the data is passed into a new function
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature); 
+    //feature is passed from html when clicked. EventEmitter will emit the string passed here
+  }
 }
